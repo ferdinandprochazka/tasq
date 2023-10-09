ec9acd5fab3ca4eb931a2d8cea468f8455bce411
ferdinandprochazka <ferdinandprochazka@proton.me>
2023-10-09 19:10:11 +0200

    Adding all main command entries

--- src/Main.c
+++ src/Main.c
@@ -8,21 +8,18 @@ const char* PROJECT_AUTHOR  = "Ferdinand Procházka";
 const char* PROJECT_LICENSE = "GNU GPLv3";
 
 void printProgramInfo();
+void printCommands();
 
 int main(int argc, char** argv)
 {
   if (argc == 1)
   {
-    printf("No command was passed!\n\n");
-    printf("list - lists all tasks\n");
-    printf("info - prints program information\n");
+    printf("Tasks\n\n");
 
-    return EXIT_FAILURE;
-  }
+    printf("1) Task\n");
+    printf("2) Task\n");
+    printf("3) Task\n");
 
-  if (strcmp(argv[1], "list") == 0)
-  {
-    printf("Tasks\n");
     return EXIT_SUCCESS;
   }
 
@@ -32,7 +29,54 @@ int main(int argc, char** argv)
     return EXIT_SUCCESS;
   }
 
-  printf("Invalid command: %s\n", argv[1]);
+  if (strcmp(argv[1], "add") == 0)
+  {
+    if (argc < 3)
+    {
+      printf("No text was provided!\n");
+      return EXIT_FAILURE;
+    }
+    return EXIT_SUCCESS;
+  }
+
+  if (strcmp(argv[1], "remove") == 0)
+  {
+    if (argc < 3)
+    {
+      printf("No ID was provided!\n");
+      return EXIT_FAILURE;
+    }
+    return EXIT_SUCCESS;
+  }
+
+  if (strcmp(argv[1], "update") == 0)
+  {
+    if (argc < 3)
+    {
+      printf("No ID was provided!\n");
+      return EXIT_FAILURE;
+    }
+    if (argc < 4)
+    {
+      printf("No text was provided!\n");
+      return EXIT_FAILURE;
+    }
+    return EXIT_SUCCESS;
+  }
+
+  if (strcmp(argv[1], "clear") == 0)
+  {
+    if (argc < 3)
+    {
+      printf("All tasks were successfully deleted!\n");
+      return EXIT_FAILURE;
+    }
+    return EXIT_SUCCESS;
+  }
+
+  printf("Invalid command: %s\n\n", argv[1]);
+  printCommands();
+
   return EXIT_FAILURE;
 }
 
@@ -41,4 +85,13 @@ void printProgramInfo()
   printf("%s %s\n", PROJECT_NAME, PROJECT_VERSION);
   printf("Created by %s\n", PROJECT_AUTHOR);
   printf("Licensed under %s\n", PROJECT_LICENSE);
+}
+
+void printCommands()
+{
+  printf("add <TEXT> - adds a task to the list\n");
+  printf("delete <ID> - deletes a task with the given ID\n");
+  printf("update <ID> <TEXT> - updates a task with the given ID\n");
+  printf("clear - deletes all tasks\n");
+  printf("info - prints the program information\n");
 }
