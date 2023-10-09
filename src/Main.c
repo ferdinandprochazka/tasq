b84995b20ad368cd7c15a53c8fca2d3f85db32d2
ferdinandprochazka <ferdinandprochazka@proton.me>
2023-10-09 18:35:56 +0200

    Printing the program info

--- src/Main.c
+++ src/Main.c
@@ -1,6 +1,22 @@
 #include <stdlib.h>
+#include <stdio.h>
+
+const char* PROJECT_NAME    = "tasq";
+const char* PROJECT_VERSION = "0.1";
+const char* PROJECT_AUTHOR  = "Ferdinand Procházka";
+const char* PROJECT_LICENSE = "GNU GPLv3";
+
+void printProgramInfo();
 
 int main()
 {
+  printProgramInfo();
   return EXIT_SUCCESS;
+}
+
+void printProgramInfo()
+{
+  printf("%s %s\n", PROJECT_NAME, PROJECT_VERSION);
+  printf("Created by %s\n", PROJECT_AUTHOR);
+  printf("Licensed under %s\n", PROJECT_LICENSE);
 }
