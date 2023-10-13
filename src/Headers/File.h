6b91bd4e13e29cd147cf326c083ee65c8ff3c780
ferdinandprochazka <ferdinandprochazka@proton.me>
2023-10-13 12:37:24 +0200

    Processing the task database file

--- src/Headers/File.h
+++ src/Headers/File.h
@@ -3,6 +3,9 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <ctype.h>
 
 void printTasks();
 
--- src/Sources/File.c
+++ src/Sources/File.c
@@ -1,5 +1,8 @@
 #include "../Headers/File.h"
 
+/**
+ * Print the tasks from a task database file.
+ */
 void printTasks()
 {
   FILE *file = fopen("data/taskdb.txt", "r");
@@ -9,8 +12,36 @@ void printTasks()
   }
 
   char lineContent[128];
+  unsigned int lineNumber = 0;
+
   while (fgets(lineContent, sizeof(lineContent), file)) {
-    printf("%s", lineContent);
+    bool done = false;
+    int lastSemicolonPosition = -1;
+
+    for (int i = 0; i < strlen(lineContent); i++) {
+      if (lineContent[i] == ';') {
+        lastSemicolonPosition = i;
+      }
+    }
+
+    if (lastSemicolonPosition == -1) continue;
+
+    char slicedString[lastSemicolonPosition];
+    strncpy(slicedString, lineContent, lastSemicolonPosition);
+
+
+    if (lastSemicolonPosition + 1 >= strlen(lineContent)) continue;
+    char statusCharacter = lineContent[lastSemicolonPosition + 1];
+
+    if (!isdigit(statusCharacter)) continue;
+
+    int status = statusCharacter - '0';
+    if (status == 1) {
+      done = true;
+    }
+
+    printf("%d) %s (%s)\n", lineNumber + 1, slicedString, done ? "Done" : "Undone");
+    lineNumber++;
   }
 
   fclose(file);
